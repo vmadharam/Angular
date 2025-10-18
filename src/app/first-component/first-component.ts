6256e8a174699f27eec1bf4449740b42492b6865
vmadharam <69879633+vmadharam@users.noreply.github.com>
2025-10-18 15:37:14 -0400

    slice

--- src/app/first-component/first-component.ts
+++ src/app/first-component/first-component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './first-component.html',
   styleUrl: './first-component.scss'
 })
-export class FirstComponent {
+export class FirstComponent implements OnInit{
+  public photos: any[] = []; // Array to store fetched data
+  private photosApiUrl = 'https://jsonplaceholder.typicode.com/photos';
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get<any[]>(this.photosApiUrl).subscribe(data => {
+          this.photos = data; // Assign fetched data to the posts array
+        });
+  }
 
 }
