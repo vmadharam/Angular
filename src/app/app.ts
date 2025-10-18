cbeeb0205458b591d6f931edc81a85bb88d276a6
vmadharam <69879633+vmadharam@users.noreply.github.com>
2025-10-18 13:04:19 -0400

    app commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,12 +12,15 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
       providedIn: 'root'
     })
+    
 export class App implements OnInit{
   public todos: any[] = []; // Array to store fetched data
   private apiUrl = 'https://jsonplaceholder.typicode.com/todos'; // Example API URL
 
   constructor(private http: HttpClient) { }
+
   protected readonly title = signal('Angular');
+
   ngOnInit(): void {
         this.http.get<any[]>(this.apiUrl).subscribe(data => {
           this.todos = data; // Assign fetched data to the posts array
